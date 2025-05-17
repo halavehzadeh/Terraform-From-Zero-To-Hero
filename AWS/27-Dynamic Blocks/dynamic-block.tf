17bb03f90b5b192b74de6759aceb2014de5e5852
halavehzadeh <halavehzadeh@outlook.com>
2025-05-17 18:00:33 -0400

    added terraform validate and updated dynamic blocks

--- AWS/27-Dynamic Blocks/dynamic-block.tf
+++ AWS/27-Dynamic Blocks/dynamic-block.tf
@@ -1,22 +1,22 @@
-# variable "sg_ports" {
-#   type        = list(number)
-#   description = "List of ingress ports"
-#   default     = [8200, 8201, 8300, 9200, 9500]
-# }
+variable "sg_ports" {
+  type        = list(number)
+  description = "List of ingress ports"
+  default     = [8200, 8201, 8300, 9200, 9500]
+}
 
-# resource "aws_security_group" "dynamicsg" {
-#   name        = "dynamcic-sgs"
-#   description = "Ingress for Vault"
+resource "aws_security_group" "dynamicsg" {
+  name        = "dynamcic-sgs"
+  description = "Ingress for Vault"
 
-#   dynamic "ingress" {
-#     for_each = var.sg_ports
-#     content {
-#       from_port   = ingress.value
-#       to_port     = ingress.value
-#       protocol    = "tcp"
-#       cidr_blocks = ["0.0.0.0/0"]
-#     }
+  dynamic "ingress" {
+    for_each = var.sg_ports
+    content {
+      from_port   = ingress.value
+      to_port     = ingress.value
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 
-#   }
+  }
 
-# }
+}
